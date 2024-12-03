43debeffcb21721ca0388160a2506e078eb70d56
SempaiDarcy <mr_darcy_1@mail.ru>
2024-12-03 23:53:40 +0500

    feat: add navbar with links and icons

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,26 @@
+import { FaHome, FaList, FaUser } from 'react-icons/fa';
+import s from './Navbar.module.scss';
+
+export const Navbar = () => {
+    return (
+        <nav className={s.navbar}>
+            <ul className={s.navList}>
+                <li className={s.navItem}>
+                    <a href="/">
+                        <FaHome /> Главная
+                    </a>
+                </li>
+                <li className={s.navItem}>
+                    <a href="/catalog">
+                        <FaList /> Каталог
+                    </a>
+                </li>
+                <li className={s.navItem}>
+                    <a href="/profile">
+                        <FaUser /> Личный кабинет
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
