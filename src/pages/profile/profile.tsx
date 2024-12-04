6ecbcc3bbaa1ca48e68dc6fb2dc1307778e4bc55
SempaiDarcy <mr_darcy_1@mail.ru>
2024-12-04 14:55:56 +0500

    feat: add profile

---
+++ src/pages/profile/profile.tsx
@@ -0,0 +1,8 @@
+export const Profile = () => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>Личный кабинет</h1>
+            <p>Здесь будут данные пользователя.</p>
+        </div>
+    );
+};
